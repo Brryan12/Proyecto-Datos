4a5cd5aa281e0bcc7b975af3cedabd531e819684
AndGSC <andrey.solis.carvajal@est.una.ac.cr>
2025-10-05 23:09:08 -0600

    Update button.py

--- src/game/button.py
+++ src/game/button.py
@@ -1,4 +1,4 @@
-class Button():
+class Button(): #clase de servicio para el menú principal, tanto para jugar como guardado y salida.
 	def __init__(self, image, pos, text_input, font, base_color, hovering_color):
 		self.image = image
 		self.x_pos = pos[0]
@@ -12,17 +12,17 @@ class Button():
 		self.rect = self.image.get_rect(center=(self.x_pos, self.y_pos))
 		self.text_rect = self.text.get_rect(center=(self.x_pos, self.y_pos))
 
-	def update(self, screen):
+	def update(self, screen): #Observa si el texto se debe actualizar
 		if self.image is not None:
 			screen.blit(self.image, self.rect)
 		screen.blit(self.text, self.text_rect)
 
-	def checkForInput(self, position):
+	def checkForInput(self, position): #Revisa si el usuario da click en el botón correspondiente
 		if position[0] in range(self.rect.left, self.rect.right) and position[1] in range(self.rect.top, self.rect.bottom):
 			return True
 		return False
 
-	def changeColor(self, position):
+	def changeColor(self, position): #cambia el color, en caso de pasar el mouse por el botón que corresponde.
 		if position[0] in range(self.rect.left, self.rect.right) and position[1] in range(self.rect.top, self.rect.bottom):
 			self.text = self.font.render(self.text_input, True, self.hovering_color)
 		else:
