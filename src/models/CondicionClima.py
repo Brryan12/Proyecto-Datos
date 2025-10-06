363228bf742a618cbd7b2bff1e020f50035a3db6
AndGSC <andrey.solis.carvajal@est.una.ac.cr>
2025-10-05 21:56:35 -0600

    Update CondicionClima.py
    Añade comentarios a condición clima.

--- src/models/CondicionClima.py
+++ src/models/CondicionClima.py
@@ -1,9 +1,13 @@
 from pydantic import BaseModel, validator
 
+
+#Tiene como propósito tomar las condiciones de clima y su intensidad (que afecta al jugador)
 class CondicionClima(BaseModel):
     condition: str
     intensity: float
 
+
+    #En caso de números que se salgan de la normalización de la intensidad, se aplica el método.
     @validator("intensity", pre=True)
     def normalizar_intensidad(cls, v):
         try:
